67bf841460a15f6c69357871e374f4b8d4673145
Lokesh <gugulothlokesh1998@gmail.com>
2024-08-08 00:21:16 +0530

    added port to .env

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const server = express()
 const bodyParser = require('body-parser');
-
+require('dotenv').config
 const userRoute = require('./src/router/userRoute');
 const expenseRoute = require('./src/router/expensRoute');
 const Error = require('./src/error/errorHandling');
 server.use(bodyParser.json());
 
-const port = 8081;
+const PORT = process.env.PORT;
 
 
 server.use('/users', userRoute)
@@ -19,6 +19,6 @@ server.use('*', Error.handleNotFound)
 server.use(Error.handleError)
 
 
-server.listen(port, () => {
-    console.log(`Server started on port : ${port}`)
+server.listen(PORT, () => {
+    console.log(`Server started on port : ${PORT}`)
 })
